ec229f9c33531d5de63e9014590783b47244c712
Ehtisham Ayaan <56442970+Ehtisham-Ayaan@users.noreply.github.com>
2022-10-31 21:58:48 +0500

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'https://ebay-listing.netlify.app']
+    origins ['http://localhost:8080', 'https://ebay-listing.netlify.app']
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
